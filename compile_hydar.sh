126fa8bde543048c08e2954ad60de1b8cc8e5e36
gmerr3 <gmerr3@illinois.edu>
2023-04-30 15:18:56 -0400

    Create separate repository for turn

--- compile_hydar.sh
+++
@@ -1,2 +0,0 @@
-javac -cp "./classes/" -d "./classes" "./src/java/xyz/hydar/ee/*.java:./src/java/xyz/hydar/turn/*.java:./src/java/xyz/hydar/app/*.java"
-java --enable-preview -cp "./classes/:./lib/*" xyz.hydar.ee.Hydar
